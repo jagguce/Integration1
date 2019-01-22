964acb846bb7f160959ebb2cae19c7771379b4b9
sangeetha <sangeetha.r2@hpe.com>
2019-01-22 15:05:20 +0530

    added simple program

---
+++ Integration1/src/add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=15,total;
+		total=a+b;
+		System.out.println("sum is "+total);
+	}
+
+}
